ff36c36adf11db7232fbc92edfabebab1c3e069f
Andrew Kotowiecki <s33577@pjwstk.edu.pl>
2026-04-17 21:13:10 +0200

    Get Request on api/Rooms. Returns the list of rooms.

--- WebApplication2/WebApplication2/Controllers/RoomsController.cs
+++ WebApplication2/WebApplication2/Controllers/RoomsController.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using WebApplication2.DTOs;
+using WebApplication2.Models;
 
 namespace WebApplication2.Controllers;
 
@@ -8,6 +10,36 @@ namespace WebApplication2.Controllers;
 public class RoomsController : ControllerBase
 {
     // GET api/rooms 
+    public IActionResult GetRooms([FromQuery] int? minCapacity, [FromQuery] bool? hasProject, [FromQuery] bool? activeOnly)
+    {
+        var query = DataStore.Rooms.AsQueryable();
+
+        if (minCapacity.HasValue)
+        {
+            query = query.Where(r => r.Capacity >= minCapacity.Value);
+        }
+
+        if (hasProject.HasValue)
+        {
+            query = query.Where(r => r.HasProjector == hasProject.Value);
+        }
+
+        if (activeOnly.HasValue && activeOnly.Value)
+        {
+            query = query.Where(r => r.IsActive);
+        }
+
+        var response = query.Select(r => new RoomResponseDto
+        {
+            Id = r.Id,
+            Name = r.Name,
+            BuildingCode = r.BuildingCode,
+            Capacity = r.Capacity,
+            HasProjector = r.HasProjector,
+            IsActive = r.IsActive,
+        }).ToList();
+        return Ok(response);
+    }
     
 
     
