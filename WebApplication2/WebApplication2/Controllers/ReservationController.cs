302a1fd101950e1dee59235aa982354ed968e1f1
Andrew Kotowiecki <s33577@pjwstk.edu.pl>
2026-04-20 19:54:33 +0200

    Get reservation/[id] and Post reservation.

--- WebApplication2/WebApplication2/Controllers/ReservationController.cs
+++ WebApplication2/WebApplication2/Controllers/ReservationController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using WebApplication2.DTOs;
 using WebApplication2.Models;
 
 namespace WebApplication2.Controllers;
@@ -31,6 +32,46 @@ public class ReservationController : ControllerBase
         return Ok(query.ToList());
         
     }
+
+    [HttpGet("{id}")]
+    public IActionResult GetReservation(int id)
+    {
+        var reservation = DataStore.Reservations.FirstOrDefault(r => r.Id == id);
+        if (reservation == null)
+        {
+            return NotFound();
+        }
+        return Ok(reservation);
+    }
+    
+    //POST /api/reservations
+    [HttpPost]
+    public IActionResult CreateReservation([FromBody] Reservation reservation)
+    {
+        
+        var room = DataStore.Rooms.FirstOrDefault(r => r.Id == reservation.RoomId);
+        
+        if (room == null)
+        {
+            return BadRequest();
+        }
+
+        if (!room.IsActive)
+        {
+            return BadRequest();
+        }
+        bool isOverlap = DataStore.Reservations.Any(r => r.RoomId == reservation.RoomId && r.Date == reservation.Date  && r.StartTime < reservation.EndTime && r.EndTime > reservation.StartTime);
+        if (isOverlap)
+        {
+            return Conflict();
+        }
+
+        reservation.Id = DataStore.GetNextReservationId();
+        DataStore.Reservations.Add(reservation);
+        return CreatedAtAction(nameof(GetReservation), new { id = reservation.Id }, reservation);
+
+    }
+    
     
     
     
