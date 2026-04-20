7e69e75f67bb0cbbe2b05a0baf7e7abee25a0168
Andrew Kotowiecki <s33577@pjwstk.edu.pl>
2026-04-20 19:30:20 +0200

    Get /api/reservation with Querable filtering

--- WebApplication2/WebApplication2/Controllers/ReservationController.cs
+++ WebApplication2/WebApplication2/Controllers/ReservationController.cs
@@ -1,6 +1,37 @@
+using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Models;
+
 namespace WebApplication2.Controllers;
 
-public class ReservationController
+
+[ApiController]
+[Route("api/[controller]")]
+public class ReservationController : ControllerBase
+
 {
+    // get /api/reservation/
+    [HttpGet]
+    public IActionResult GetReservation([FromQuery] DateOnly? date, [FromQuery] int? roomId, [FromQuery] string? status)
+    {
+        var query = DataStore.Reservations.AsQueryable();
+        if (date.HasValue)
+        {
+            query = query.Where (r => r.Date == date.Value);
+        }
+
+        if (roomId.HasValue)
+        {
+            query = query.Where(r => r.RoomId == roomId.Value);
+        }
+
+        if (!string.IsNullOrEmpty(status))
+        {
+            query = query.Where(r => r.Status == status);
+        }
+        return Ok(query.ToList());
+        
+    }
+    
+    
     
 }
